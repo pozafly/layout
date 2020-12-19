648618a09a6a0673a3046a09c1064c65225e7f8d
HwangSunTae <pozafly@gmail.com>
2020-12-19 11:05:48 +0900

    init & layout 1

---
+++ src/components/Layout1.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrap">
+    <div class="header">
+      header
+    </div>
+    <div class="content">content</div>
+    <div class="footer">footer</div>
+  </div>
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+.wrap {
+  min-width: 800px;
+  text-align: center;
+}
+.header {
+  height: 100px;
+  background: lightgreen;
+}
+.content {
+  max-width: 1200px;
+  /* 블럭요소를 가운데 정렬할 수 있는 margin 0 auto */
+  margin: 0 auto;
+  height: 300px;
+  background: lightsalmon;
+}
+.footer {
+  height: 100px;
+  background: lightblue;
+}
+</style>
+
+<script>
+export default {};
+</script>
